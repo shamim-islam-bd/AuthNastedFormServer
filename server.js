c42c3f09e5bfd096b1a4196cc4ea345fa96012ec
Shamim <shamimislam@gmail.com>
2023-05-03 10:33:35 +0600

    deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ require("dotenv").config();
 // Database connection
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.MONGO_LOCAL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
